9461ee0a6201f668d2f3df442d7138f43076d5c0
sdheeraj <sdheeraj@trainee.nrifintech.comgit config --global user.email sdheeraj@trainee.nrifintech.com>
2024-02-19 16:24:45 +0530

    Till  @for & @if

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
@@ -11,4 +12,18 @@ export class UserProfileComponent {
   name = "ramesh"
   status = "single"
   salary = 40000
+  isBtnDisabled = true
+  inputVal = "test"
+  
+  users = [
+    {name:"ramesh", isSingle: true, salary:10000},
+    {name:"suresh", isSingle: false, salary:60000},
+    {name:"nitesh", isSingle: true, salary:90000},
+  ]
+  // onChange(e:Event) {
+  //   const value = (e.target as HTMLInputElement).value
+  //   console.log(value)
+  //   this.inputVal = value
+  // This was helper function for one-way data binding
+ //}
 }

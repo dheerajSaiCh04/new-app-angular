a798d72a65459f11bfa802dff27cf53195851c86
sdheeraj <sdheeraj@trainee.nrifintech.comgit config --global user.email sdheeraj@trainee.nrifintech.com>
2024-02-19 15:35:43 +0530

    Initial changes on angular project

---
+++ src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  name = "ramesh"
+  status = "single"
+  salary = 40000
+}

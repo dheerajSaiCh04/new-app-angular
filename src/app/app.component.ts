7edcf284185f6f89e6b08546d6da304a596102af
sdheeraj <sdheeraj@trainee.nrifintech.comgit config --global user.email sdheeraj@trainee.nrifintech.com>
2024-02-20 10:47:41 +0530

    Till pasing data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserProfileComponent],
+  imports: [ UserProfileComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+    users = [
+    {name:"ramesh", isSingle: true, salary:10000},
+    {name:"suresh", isSingle: false, salary:60000},
+    {name:"nitesh", isSingle: true, salary:90000},
+  ]
 }
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,29 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, booleanAttribute, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+function formatName(value:string) {
+  return "Hi " + value
+}
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-  name = "ramesh"
-  status = "single"
-  salary = 40000
-  isBtnDisabled = true
-  inputVal = "test"
+
+  // name = "ramesh"
+  // status = "single"
+  // salary = 40000
+  // isBtnDisabled = true
+  // inputVal = "test"
   
-  users = [
-    {name:"ramesh", isSingle: true, salary:10000},
-    {name:"suresh", isSingle: false, salary:60000},
-    {name:"nitesh", isSingle: true, salary:90000},
-  ]
+  // users = [
+  //   {name:"ramesh", isSingle: true, salary:10000},
+  //   {name:"suresh", isSingle: false, salary:60000},
+  //   {name:"nitesh", isSingle: true, salary:90000},
+  // ]
   // onChange(e:Event) {
   //   const value = (e.target as HTMLInputElement).value
   //   console.log(value)
   //   this.inputVal = value
   // This was helper function for one-way data binding
  //}
+  @Input({alias:"userName", transform: formatName}) name = "";
+  @Input({transform:booleanAttribute}) isSingle!: boolean;
+  @Input({transform:numberAttribute}) salary!: number;
 }
